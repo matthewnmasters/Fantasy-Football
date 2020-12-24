3764f546bbfa2342ed12a46f453c6c5adb17027e
matthewnmasters <31800923+matthewnmasters@users.noreply.github.com>
2020-12-23 23:32:35 -0500

    Created and Uploaded Bare-Bones
    Bare-bones version created and uploaded to shinyapps.io

--- App.R
+++ App.R
@@ -7,26 +7,59 @@
 library(shiny)
 library(dplyr)
 library(ggplot2)
+library(DT)
 
-#ui<-
+ui<- pageWithSidebar(
+  headerPanel('Emory Bonerz Fantasy Football Records and Visualization'),
+  sidebarPanel(
+    uiOutput("playersSelected"),
+    uiOutput("yearsSelected"),
+    uiOutput("outcomeVariable")
+  ),
+  mainPanel(
+    DT::dataTableOutput("tableOut"),
+    "put graph code here"
+  )
+)
 
 
 server<- function(input, output, session) {
   
-  longData<- reactive({
+  longData<- read.csv("fantasyStats.csv")
+  
+  filteredData<- reactive({
     
-    #reactive to:
-    input$playersSelected
-    input$yearsSelected
+    newData <- longData %>%
+      filter(Name %in% input$playersSelected, Season %in% input$yearsSelected)
     
-    rawData <- read.csv("C:/Users/matth/Documents/GitHub/Fantasy-Football/fantasyStats.csv") %>%
-      filter(Name %in% input$playersSelected, Season %in% yearsSelected)
-    
-    return(rawData)
+    return(newData)
     
   })
   
+  output$tableOut <- DT::renderDataTable(
+    
+    filteredData(),
+    options= list(pageLength= 12)
+    
+  )
   
+  output$playersSelected <- renderUI({
+    
+    checkboxGroupInput(inputId = "playersSelected", label= "Filter Players:", choices= unique(longData$Name), selected =  unique(longData$Name))
+    
+  })
+  
+  output$yearsSelected <- renderUI({
+    
+    checkboxGroupInput(inputId = "yearsSelected", label= "Filter Years:", choices= unique(longData$Season), selected =  unique(longData$Season))
+    
+  })
+  
+  output$outcomeVariable <- renderUI({
+    
+    selectInput(inputId = "outcomeVariable", label = "Y Axis for Graph:", choices = c("Wins" = "Win", "Losses" = "Loss", "Points For" = "PF", "Points Against" = "PA", "Yearly Ranking" = "Position", "Number of Moves" = "Moves", "Win Percentage" =  "WinPerc", "Point Differential" =  "PtDiff"))
+    
+  })
   
 }
 
